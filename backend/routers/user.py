568d9f23256b017890ca584f4428f7757eeaa97f
Eremina23k <ereminadarya2005@mail.ru>
2025-07-17 11:38:44 +0600

    07.07.2025 11:38

--- backend/routers/user.py
+++ backend/routers/user.py
@@ -4,3 +4,13 @@ from typing import List
 from database import get_db  # функция, возвращающая сессию БД
 from crud.user import create_user, get_user, get_user_by_login, update_user, delete_user
 from schemas import UserIn, UserOut
+
+router = APIRouter()
+
+@router.post("/", response_model=UserOut)
+def api_create_user(user: UserIn, db: Session = Depends(get_db)):
+    if get_user_by_login(db, user.login):
+        raise HTTPException(status_code=400, detail="Login already exists")
+
+    db_user = create_user(db, user)  # пароль будет захеширован внутри create_user
+    return db_user
